3dc551fb31a7f38b874572f50e58483cd7d9851a
Prueba <laveslofu@gmail.com>
2025-02-24 22:19:06 -0500

    Se agrego un arreglo para guardar a los sorteados, se modifico la funcion sorteaAmigo() y se modifico la funcion actualizarListaAmigos() para mostrar con tachado a los ya sorteados

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let amigosSorteados = [];
 
 document.getElementById('amigo').addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
         document.querySelector(".button-add").click();
     }
 });
+
+
 
 function agregarAmigo () {
     let input = document.getElementById('amigo');
@@ -23,7 +26,10 @@ function actualizarListaAmigos () {
     listaAmigos.innerHTML = '';
     for (let i = 0; i < amigos.length; i++) {
         let nombreAmigo = document.createElement('li');
-        nombreAmigo.textContent = amigos[i];
+        nombreAmigo.innerHTML = amigos[i];
+        if (amigosSorteados.includes(i)) {
+            nombreAmigo.innerHTML = "<s>" + nombreAmigo.innerHTML + "</s>";
+        }
         listaAmigos.appendChild(nombreAmigo);
     }
 }
@@ -41,8 +47,18 @@ function validarNombre (nombre) {
 }
 
 function sortearAmigo () {
-    if(amigos.length != 0){
-        let indiceAmigo = Math.floor(Math.random() * amigos.length);
+    if (amigosSorteados.length == amigos.length) {
+        document.getElementById('resultado').innerHTML = 'Todos los nombres fueron sorteados.';
+        return;
+    }
+    if (amigos.length != 0){
+        let indiceAmigo = 0;
+
+        do {
+            indiceAmigo = Math.floor(Math.random() * amigos.length);
+        } while (amigosSorteados.includes(indiceAmigo));
+        amigosSorteados.push(indiceAmigo);
+
         document.getElementById("listaAmigos").innerHTML = '';
         document.getElementById('resultado').innerHTML = `El amigo secreto sorteado es: ${amigos[indiceAmigo]}`;
     } else {
